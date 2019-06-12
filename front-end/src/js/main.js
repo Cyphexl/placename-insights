9fc07dd213471d06cd4dab41a6ffaad238cb1c7d
Tzingtao Chow <Cyphexl@outlook.com>
2019-06-12 16:30:05 +0200

    add-switch-language-automatically

--- front-end/src/js/main.js
+++ front-end/src/js/main.js
@@ -16,6 +16,18 @@ const main = () => {
     })
   })
 
+  // Detect browser language and set automatically
+  switch(getFirstBrowserLanguage()) {
+    case "zh-CN":
+      tran.setLang('zh')
+      break
+    case "fr-FR":
+      tran.setLang('fr')
+      break
+    default:
+      tran.setLang('en')
+  }
+
   // Carousel Gallery Initiate
   let statsSiema = new Siema({
     selector: '.siema',
@@ -50,6 +62,43 @@ const main = () => {
       })
       .catch(error => console.error('Placename Submit Error.', error))
   })
+}
+
+function getFirstBrowserLanguage() {
+  var nav = window.navigator,
+    browserLanguagePropertyKeys = ['language', 'browserLanguage', 'systemLanguage', 'userLanguage'],
+    i,
+    language,
+    len,
+    shortLanguage = null;
+
+  // support for HTML 5.1 "navigator.languages"
+  if (Array.isArray(nav.languages)) {
+    for (i = 0; i < nav.languages.length; i++) {
+      language = nav.languages[i];
+      len = language.length;
+      if (!shortLanguage && len) {
+        shortLanguage = language;
+      }
+      if (language && len > 2) {
+        return language;
+      }
+    }
+  }
+
+  // support for other well known properties in browsers
+  for (i = 0; i < browserLanguagePropertyKeys.length; i++) {
+    language = nav[browserLanguagePropertyKeys[i]];
+    len = language.length;
+    if (!shortLanguage && len) {
+      shortLanguage = language;
+    }
+    if (language && len > 2) {
+      return language;
+    }
+  }
+
+  return shortLanguage;
 }
 
 function setReport(data) {
